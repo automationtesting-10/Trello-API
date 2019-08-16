9887c3e0067c388a400dbcd9b89f3958de1a2a37
unknown <aburgos.jala@gmail.com>
2019-08-16 03:31:09 -0400

    Json Converter class added

---
+++ src/main/java/com/foundation/trello/util/JsonConverter.java
@@ -0,0 +1,44 @@
+package com.foundation.trello.util;
+
+import com.foundation.trello.model.Board;
+import com.foundation.trello.model.Card;
+import com.google.gson.Gson;
+
+/**
+ * Implements JsonConverter abstract class which is used to convert json records in DB.
+ *
+ * @author Melissa Román
+ * @version 1.0
+ */
+public abstract class JsonConverter {
+    /**
+     * Converts a json to a Criteria instance according on given type.
+     * @param json - String corresponding to json.
+     * @param type - Object's type.
+     * @return Instance of Object class according on given type.
+     */
+    public static Object jsonToObject(String json, String type) {
+        Gson gson = new Gson();
+        switch (type) {
+            case "Board":
+                return gson.fromJson(json, Board.class);
+            case "Card":
+                return gson.fromJson(json, Card.class);
+            case "Other":
+                return gson.fromJson(json, Other.class);
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * Converts an instance of ConvertCriteria class to json.
+     * @param object - Object to be convert to json.
+     * @return json corresponding to given Object.
+     */
+    public static String convertObjectToJson(Object object) {
+        Gson gson = new Gson();
+        String json = gson.toJson(object);
+        return json;
+    }
+}
