d1f874d52c95bd9bf72355b11aa0e058280a4771
LIMBERT <limbert.vargas@fundacion-jala.org>
2019-08-19 00:33:59 -0400

    adding list acceptance test and step

---
+++ src/test/java/com/foundation/trello/step/ListAcceptanceStepDef.java
@@ -0,0 +1,52 @@
+package com.foundation.trello.step;
+
+import com.foundation.trello.util.Authentication;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import gherkin.deps.com.google.gson.JsonObject;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.json.JSONObject;
+
+import static io.restassured.RestAssured.given;
+
+public class ListAcceptanceStepDef {
+    RequestSpecification requestSpecification;
+    String endpoint = null;
+    JSONObject bodyJson;
+    String json = null;
+    Response response;
+
+    @Given("I create a LIST request to {string} endpoint")
+    public void i_create_a_LIST_request_to_endpoint(String endpoint) {
+        requestSpecification = Authentication.getInstance().getRequestSpecification();
+        this.endpoint = endpoint;
+    }
+
+    @Given("I set up the data:")
+    public void i_set_up_the_data(String paramName) {
+        bodyJson = new JSONObject(paramName);
+        this.json = paramName;
+    }
+
+    @When("I sent the request")
+    public void i_sent_the_request() {
+        response = given().contentType(ContentType.JSON).spec(requestSpecification).body(json).when().post(endpoint);
+    }
+
+    @Then("I get a {int} status code as response")
+    public void i_get_a_status_code_as_response(int status) {
+        Assert.assertEquals(status, response.getStatusCode());
+    }
+
+    @Then("I verify the list schema")
+    public void i_verify_the_list_schema() {
+    }
+
+    @Then("Sent Data should be the same info of the response")
+    public void sent_data_should_be_the_same_info_of_the_response(){
+    }
+}
---
+++ src/test/resources/feature/list_acceptance.feature
@@ -0,0 +1,19 @@
+Feature: Acceptance test of "list", only Create
+
+  Scenario: Create new list with name
+    Given I create a LIST request to "/lists" endpoint
+      And I set up the data:
+      """
+        {
+          "id": "5d5a0d89e6a38889fae28c63",
+          "name": "List1",
+          "closed": false,
+          "idBoard": "5d5a0d2fb868e0188cde9b8d",
+          "pos": 16384,
+          "limits": {}
+        }
+      """
+    When I sent the request
+    Then I get a 200 status code as response
+      And I verify the list schema
+      And Sent Data should be the same info of the response
