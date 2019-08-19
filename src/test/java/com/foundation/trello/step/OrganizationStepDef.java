51a3c4806c95d6e1f95cbd2faf2ce576b613caa2
root <root@andres-ubuntu.novalocal>
2019-08-19 18:30:26 -0400

    organization base scenarios added

---
+++ src/test/java/com/foundation/trello/step/OrganizationStepDef.java
@@ -0,0 +1,9 @@
+package com.foundation.trello.step;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import io.restassured.response.Response;
+
+public class OrganizationStepDef {
+}
