2499691eea64f2b637fe7c57c80e31ec63af073a
Josue <jrg.ikaro@gmail.com>
2019-08-21 13:36:54 -0400

    regex improved

--- src/main/java/com/foundation/trello/util/Regex.java
+++ src/main/java/com/foundation/trello/util/Regex.java
@@ -11,30 +11,30 @@ import java.util.regex.Pattern;
 public final class Regex {
     private static Regex ourInstance;
     private String string;
-    private String id;
+    private String[] ids;
     private String result;
 
     /**
      * This method constructor initialize the parameters.
      *
      * @param string The oldString parameter defines the endpoint input.
-     * @param id The id parameter defines the id of the card.
+     * @param ids The id parameter defines the list of id.
      */
-    private Regex(String string, String id) {
+    private Regex(String string, String[] ids) {
         this.string = string;
-        this.id = id;
+        this.ids = ids;
     }
 
     /**
      * This method verify if of object Regex was created, if not creates it.
      *
      * @param string The string parameter defines the endpoint input.
-     * @param id The id parameter defines the id of the card.
+     * @param ids The id parameter defines the list of id.
      * @return Regex.
      */
-    public static Regex getInstance(String string, String id) {
+    public static Regex getInstance(String string, String[] ids) {
         if (ourInstance == null) {
-            ourInstance = new Regex(string, id);
+            ourInstance = new Regex(string, ids);
         }
         return ourInstance;
     }
@@ -45,9 +45,11 @@ public final class Regex {
      * @return new endpoint with id.
      */
     public String getString() {
-        Pattern pattern = Pattern.compile("\\{id\\}");
-        Matcher matcher = pattern.matcher(string);
-        result = matcher.replaceAll(id);
+        Pattern pattern = Pattern.compile("\\{(.*?)\\}");
+        for(int index = 0; index < ids.length; index++){
+            Matcher matcher = pattern.matcher(string);
+            string = matcher.replaceFirst(ids[index]);
+        }
         return result;
     }
 }
